410f8349ae06e785d9b524951a241e9dce19def5
faof26 <fabien.marot@gmail.com>
2014-07-28 09:40:30 +0200

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -23,3 +23,44 @@ void game::update(){
 
 
 }
+/*
+bool colision(){
+	
+	int i1 = _p->position.x/40;
+	int i1 = _p->position.y/40;
+	int i2 = (_p->position.x + _p->position.myWidth -1)/40;
+	int i2 = (_p->position.y + _p->position.myHeight -1)/40;
+	
+	int i,j;
+   	for(i=i1;i<=i2;i++)
+   	{
+     		for(j=j1;j<=j2;j++)
+     		{
+        		 if (TileIsDure(i,j))
+             			return true;
+     		}
+	}
+	 return false;
+}
+
+bool colision(int x,int y){
+	
+	int i1 = x/40;
+	int i1 = y/40;
+	int i2 = (x + _p->position.myWidth -1)/40;
+	int i2 = (y + _p->position.myHeight -1)/40;
+	
+	int i,j;
+   	for(i=i1;i<=i2;i++)
+   	{
+     		for(j=j1;j<=j2;j++)
+     		{
+        		 if (TileIsDure(i,j))
+             			return true;
+     		}
+	}
+	 return false;
+} */
+
+
+
