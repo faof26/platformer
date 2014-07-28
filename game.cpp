6136f2ca0d00922b91d3f63ea13eeac358abbcf9
faof26 <fabien.marot@gmail.com>
2014-07-28 15:07:49 +0200

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -17,7 +17,67 @@ game::game(player *_p, world *_w){
 void game::update(){
 	//calcul de la nouvelle position du joueur
 	//...
+	/*
+	bool testx;
+	bool testy;
+	testx=colision(_p->position.x+_p->vitesse.x,_p->position.y);
+	testy=colision(_p->position.x,_p->position.y+_p->vitesse.y);
+	if(testx==true)
+	{
+		if(testy==true)
+		{
+			_p->position.x=_p->position.x+_p->vitesse.x;
+			_p->position.y=_p->position.y+_p->vitesse.y;
+		}
+		else
+		{
+			_p->position.x=_p->position.x+_p->vitesse.x;
+			_p->position.y=_p->position.y+(40 - ((_p->position.y + _p->myHeight)%40));
+			if(_p->isInAir==true && _p.vitesse.y<0)
+			{
+				_p->isInAir=false;
+				_p->vitesse.y=0;
+			}
+			else
+			{
+				_p->vitesse.y=-1;	
+			}
+			
+			
+		}
+	}
+	else
+	{
+		if(testy==true)
+		{
+			_p->position.y=_p->position.y+_p->vitesse.y;
+			_p->position.x=_p->position.x+(40 - ((_p->position.x + _p->myWidth)%40));
+			_p->vitesse.x=0;
+		}
+		else
+		{
+			_p->position.y=_p->position.y+(40 - ((_p->position.y + _p->myHeight)%40));
+			if(_p->isInAir==true && _p.vitesse.y<0)
+			{
+				_p->isInAir=false;
+				_p->vitesse.y=0;
+			}
+			else
+			{
+				_p->vitesse.y=-1;	
+			}
+			_p->position.x=_p->position.x+(40 - ((_p->position.x + _p->myWidth)%40));
+			_p->vitesse.x=0;
+		}
+	}
 
+
+	if(_p->isInAir==true)
+	{
+			_p->vitesse.y--;
+	}
+		
+*/
 	//collisions
 	//...
 
